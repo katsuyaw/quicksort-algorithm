ea23ef5449d12e370d92d73e106f5275a973293e
katsuyaw <kw@kmail.me>
2021-04-12 11:45:24 -0700

    Fix spacing

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,84 +1,65 @@
 #include <iostream>
 
 template<class T>
-void swap(T* a, T* b){
-    T temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(T& a, T& b) {  
+    T temp = a;
+    a = b;
+    b = temp;
 }
- 
-int c=1;
-template<class T>
-int partition (T* array, int start, int end)
-{   
-    c++;
-    // pivot is the last element
-    T pivot = array[end];
-    // i is the most left, j is the most right index
-    int i = start, j = end-1;
 
-    // condition: i < j
-    while(i<j){
-        // compare the left value with pivot
-        // increment until find greater value
-        while(array[i]<=pivot){
+template<class T>
+int partition (T* arr, int start, int end) {
+    // set pivot at the end
+    int pivot = arr[end];
+    int i = (start - 1);
+ 
+    for (int j = start; j <= end - 1; j++) {
+        // element < pivot
+        if (arr[j] < pivot){
+            // increment i(smaller) and swap with j
             i++;
-        }
-        // i could have been incremented
-        while(pivot<array[j] && i<j){
-            j--;
-        }
-        // i != j swap still should happen
-        if(i<j){
-            swap(&array[i], &array[j]);
-
+            swap(arr[i], arr[j]);
         }
     }
-    // i = j can happen for multiple reasons
-    // 1. i < pi and pi < j, pass the test, then i and j meet in the middle
-    if(i<=j && array[j] > array[end])
-        swap(&array[j], &array[end]);
-
-
-
-    return j;
-
-        
+    // place pivot
+    swap(arr[i + 1], arr[end]);
+    return (i + 1);
 }
- 
-
 
 template<class T>
-void quickSort(T* array, int start, int end)
-{
+void quickSort(T* arr, int start, int end) {   
     if (start < end){
-        int pi = partition(array, start, end);
-        quickSort(array, start, pi - 1);
-        quickSort(array, pi + 1, end);
+        // index of pivot
+        int index = partition(arr, start, end);
+        // quicksort upperhalf
+        quickSort(arr, start, index-1);
+        // quicksort lowerhalf
+        quickSort(arr, index+1, end);
     }
 }
+
 
 
 int main() {
     int size = 10;
-    int array[size];
+    int arr[size];
     int randomVal;
     srand(time(0));
 
     std::cout << "|Before Sort|" << std::endl;
 
 
-    for(int i; i<size; i++){
+    for(int i; i<size; i++) {
         randomVal = rand() % 100;
-        array[i] = randomVal;
-        std:: cout << array[i] << " ";
+        arr[i] = randomVal;
+        std:: cout << arr[i] << " ";
     }
 
     std::cout << "\n|After Sort|" << std::endl;
-    quickSort(array, 0, size-1);
+    quickSort(arr, 0, size-1);
 
     // print array
-    for(int i: array){
+    for(int i: arr) {
         std::cout << i << " "; 
     }
 
